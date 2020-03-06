8983b4bb943cdf20a99b6ebfe422c03e21199c05
Elie Bizimana <elie@iMacs-iMac.local>
2020-03-05 19:14:39 -0500

    Finished cleanning up home page

--- routes/webRoutes.js
+++ routes/webRoutes.js
@@ -1,30 +1,52 @@
 var express = require("express"),
-router = express.Router()
+    router = express.Router(),
+    Concession = require("../models/concession")
 
-router.get("/login", function(req,res){
+router.get("/login", function (req, res) {
     res.render("login")
 })
 
 
-router.get("/about", function(req,res){
+router.get("/about", function (req, res) {
     res.render("about")
 })
 
-router.get("/home", function(req,res){
-    res.render("home")
+router.get("/home", function (req, res) {
+    Concession.find({}, function(err, allusers){
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("home", { users : allusers });
+        }
+    })
 })
 
-router.post("/concession", function(req, res){
+router.post("/concession", function (req, res) {
+    // we got the values from the form 
     var name = req.body.studentName,
         balance = req.body.balance,
         picture = req.body.picture;
+
+    var newUser = {
+        name: name,
+        balance: balance,
+        picture: picture
+    }
+
+    Concession.create(newUser, function(err, user){
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/home");
+        }
+    })
 })
 
-router.get("/show", function(req,res){
+router.get("/show", function (req, res) {
     res.render("show")
 })
 
-router.get("/new", function(req,res){
+router.get("/new", function (req, res) {
     res.render("new")
 })
 
