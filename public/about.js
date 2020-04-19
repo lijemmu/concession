9dda0b0709704e33830fce98cb3472a050deb1a1
lijemmu <emmanuelermias@gmail.com>
2020-04-19 02:26:21 -0400

    Working on About page animation

--- public/about.js
+++ public/about.js
@@ -1,22 +1,25 @@
-$(".test").click(function(){
-    alert("here")
-})
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    navigation: true,
+    onLeave: (origin,destination,direction) => {
+        const section = destination.item
+        const title = section.querySelector('h1');
+        const tl = new TimelineMax({delay: 0.5});
+        tl.fromTo(title,0.5, {y: '50', opacity: 0}, { y:0, opacity:1} )
+
+
+        if(destination.index === 1) {
+            const chairs = document.querySelectorAll('.chair');
+            const description = document.querySelector('.description');
+
+            tl.fromTo(chairs, 0.7, {x: '100%'}, {x: '-35%'})
+              .fromTo(description, 1, {x: '-120%'}, {x: '0%'})
+              .fromTo(chairs[0], 2, { opacity:1 }, {opacity:1 })
+              .fromTo(chairs[1], 2, { opacity:0 }, {opacity:1 })
+              .fromTo(chairs[2], 2, { opacity:0 }, {opacity:1 })
 
 
 
-const flightPath = {
-  curviness: 1.25,
-  autoRotate: true,
-  values:[
-      { x: 100, y: -20 }, { x: 300, y:10 }
-  ]
-};
-
-const tween = new TimelineLite();
-
-tween.add(
-  TweenLite.to(".paper-plane", 1, {
-      bezier: flightPath,
-      ease: Power1.easeInOut
-  })
-);
+        }
+    }
+});
